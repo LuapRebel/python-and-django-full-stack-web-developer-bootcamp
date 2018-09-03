a8c458a815ae738652966c0123b67f1d7b98d5ca
howard <paulleber23@gmail.com>
2018-09-03 15:07:44 -0700

    Adds advanced_section

---
+++ advanced_section/advcbv/basic_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION'
+        return context
